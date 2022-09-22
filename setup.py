5bd2c8c36837f45236a9b4243dfc684d8ed1f10b
davidprush <davidprush@gmail.com>
2022-09-22 02:45:08 -0400

    Updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='keycollator',
-    version='0.0.3',
+    version='0.0.4',
     long_description=long_description,
     py_modules=[
         'cli',
